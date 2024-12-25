26927bd19ecabd3c8562d645029c41e194fec3ef
sergeodesico <sergio.deleon@cimat.mx>
2024-12-25 16:16:10 -0600

    automatize pcd alignment

--- scripts/pcd_align.sh
+++ scripts/pcd_align.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 GPU_ID=0
-DATA_BASE_PATH="/media/jbhayet/Data/datasets/BOP_CHALLENGE/hope/onboarding_static"
+DATA_ROOT_PATH="/media/jbhayet/Data/datasets/BOP_CHALLENGE/hope/onboarding_static"
 DATASETS=(
     hope
 )
@@ -19,24 +19,27 @@ FACES=(
     up
 )
 
-# Number of training iterations (if needed in the future steps)
-gs_train_iter=15000
 
 for DATASET in "${DATASETS[@]}"; do
     for OBJECT_ID in "${OBJECT_IDS[@]}"; do
         for N_VIEW in "${N_VIEWS[@]}"; do
 
-            SOURCE_PATH = "data/${DATASET}/obj_${OBJECT_ID:06d}/down/${N_VIEW}_views                # ----- (1) Key-frame selection -----
-                CMD_PCD_A="python ./pcd_align.py \
-                --data_base_path ${DATA_BASE_PATH} \
-                --dataset_name ${DATASET} \
-                --object_id ${OBJECT_ID} \
-                --n_views ${N_VIEW}"
+            DATA_BASE_PATH="./data/${DATASET}/obj_$(printf '%06d' ${OBJECT_ID})"
+            # ----- Point Cloud Alignment -----
+            CMD_PCD_A="python ./pcd_align.py \
+            --source_path=${DATA_BASE_PATH}/down/${N_VIEW}_views/sparse/0/points3D.ply \
+            --target_path=${DATA_BASE_PATH}/up/${N_VIEW}_views/sparse/0/points3D.ply \
+            --output_path=${DATA_BASE_PATH}/align \
+            --masked_target_path=${DATA_BASE_PATH}/up/${N_VIEW}_views/masked_images \
+            --masked_source_path=${DATA_BASE_PATH}/down/${N_VIEW}_views/masked_images \
+            --target_camera_path=${DATA_BASE_PATH}/up/${N_VIEW}_views/sparse/0/cameras.txt \
+            --source_camera_path=${DATA_BASE_PATH}/down/${N_VIEW}_views/sparse/0/cameras.txt \
+            --target_image_path=${DATA_BASE_PATH}/up/${N_VIEW}_views/sparse/0/images.txt \
+            --source_image_path=${DATA_BASE_PATH}/down/${N_VIEW}_views/sparse/0/images.txt"
 
-                echo "========= ${DATASET}: Key-frame selection (${FACE}) ========="
-                eval $CMD_KF
+            echo "========= ${DATASET}: Key-frame selection ========="
+            eval $CMD_PCD_A
         done
     done
 done
 
-python pcd_align.py --source_path=./data/hope/obj_000001/down/7_views/sparse/0/points3D.ply --target_path=./data/hope/obj_000001/up/7_views/sparse/0/points3D.ply --output_path=./data/hope/obj_000001/align --masked_target_path=./data/hope/obj_000001/up/7_views/masked_images --masked_source_path=./data/hope/obj_000001/down/7_views/masked_images --target_camera_path=./data/hope/obj_000001/up/7_views/sparse/0/cameras.txt --source_camera_path=./data/hope/obj_000001/down/7_views/sparse/0/cameras.txt --target_image_path=./data/hope/obj_000001/up/7_views/sparse/0/images.txt --source_image_path=./data/hope/obj_000001/down/7_views/sparse/0/images.txt
