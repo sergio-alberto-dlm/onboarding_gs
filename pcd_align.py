e837886a604ee5bfd5ec4dbb08cf7f70ec80a051
sergeodesico <sergio.deleon@cimat.mx>
2025-01-09 11:56:37 -0600

    fix cameras alignment

--- pcd_align.py
+++ pcd_align.py
@@ -50,7 +50,7 @@ def align_point_clouds(source_path, target_path, threshold):
     # Transform source point cloud
     source.transform(result_icp.transformation)
 
-    return source, target, result_icp.transformation
+    return source, target, result_icp.transformation, centroid
 
 def merge_masked_images(source_path, target_path, output_path):
     os.makedirs(output_path, exist_ok=True)
@@ -59,7 +59,7 @@ def merge_masked_images(source_path, target_path, output_path):
     target_images = sorted(os.listdir(target_path))
     for i, image in enumerate(target_images):
         src = os.path.join(target_path, image)
-        dest = os.path.join(output_path, f"frame_{i:02d}.png")
+        dest = os.path.join(output_path, f"frame_{i:02d}.jpg")
         shutil.move(src, dest)
 
     # Move and rename images from source, continuing numbering
@@ -67,7 +67,7 @@ def merge_masked_images(source_path, target_path, output_path):
     offset = len(target_images)
     for i, image in enumerate(source_images):
         src = os.path.join(source_path, image)
-        dest = os.path.join(output_path, f"frame_{i + offset:02d}.png")
+        dest = os.path.join(output_path, f"frame_{i + offset:02d}.jpg")
         shutil.move(src, dest)
 
     # Remove original directories
@@ -86,47 +86,36 @@ def merge_intrinsics_and_extrinsics(target_camera_path, source_camera_path,
     num_cameras_source = len(open(source_camera_path).readlines())
 
     with open(output_camera_path, 'w') as file:
-        for _ in range(num_cameras_target + num_cameras_source):
-            file.writelines(target_camera_data[0])  # Write identical intrinsics line for each camera
+        parts = target_camera_data[0].strip().split()
+        for idx in range(num_cameras_target + num_cameras_source):
+            file.writelines(f"{idx+1} " + " ".join(parts[1:]) + "\n")  # Write identical intrinsics line for each camera
 
     # Merge images.txt
     target_lines = open(target_image_path).readlines()
     source_lines = open(source_image_path).readlines()
 
-    def save_colmap_images(poses, images_file, train_img_list):
-        with open(images_file, 'w') as f:
-            for i, pose in enumerate(poses, 1):  # Starting index at 1
-                pose = np.linalg.inv(pose)
-                R = pose[:3, :3]
-                t = pose[:3, 3]
-                q = rotmat2qvec(R)  # Convert rotation matrix to quaternion
-                f.write(f"{i} {q[0]} {q[1]} {q[2]} {q[3]} {t[0]} {t[1]} {t[2]} {i} {train_img_list[i-1]}")
-                f.write("\n")
-
     with open(output_image_path, 'w') as outfile:
         frame_offset = 0
         for line in target_lines:
             outfile.write(line)
-            if not line.strip(): frame_offset+=1 
-
+            frame_offset+=1
         for i, line in enumerate(source_lines):
             parts = line.strip().split()
             if len(parts) > 0:
-                index = int(parts[0]) + frame_offset
+                index = i + frame_offset 
                 qvec = np.array(parts[1:5], dtype=float)
                 tvec = np.array(parts[5:8], dtype=float)
                 R = qvec2rotmat(qvec)  # Convert quaternion to rotation matrix
-                pose = np.vstack([np.hstack([R, tvec.reshape(3, -1)]), np.array([0, 0, 0, 1])])
-                pose = np.linalg.inv(pose)
+                pose = np.vstack([np.hstack([R, tvec.reshape(3, -1)]), np.array([0, 0, 0, 1])]) # world-to-camera pose 
+
                 # Apply transformation
-                updated_pose = source_transform @ pose
-                updated_pose = np.linalg.inv(updated_pose)
+                updated_pose = pose @ np.linalg.inv(source_transform) 
                 updated_R, updated_t = updated_pose[:3, :3], updated_pose[:3, 3]
                 updated_q = rotmat2qvec(updated_R)
 
                 frame_name = f"frame_{index:02d}.jpg"
-                outfile.write(f"{index} {updated_q[0]} {updated_q[1]} {updated_q[2]} {updated_q[3]} {updated_t[0]} {updated_t[1]} {updated_t[2]} {index} {frame_name}\n")
-                outfile.write("\n")
+                outfile.write(f"{index + 1} {updated_q[0]} {updated_q[1]} {updated_q[2]} {updated_q[3]} {updated_t[0]} {updated_t[1]} {updated_t[2]} {index + 1} {frame_name}\n")
+                #outfile.write("\n")
 
 def concatenate_point_clouds(source, target):
     # Load vertices and colors from source and target
@@ -164,7 +153,7 @@ def main():
     args = parser.parse_args()
 
     # Align point clouds
-    source, target, transformation = align_point_clouds(
+    source, target, transformation, centroid_source = align_point_clouds(
         args.source_path,
         args.target_path,
         args.threshold
@@ -186,8 +175,13 @@ def main():
                   [0, -1,  0, 0],
                   [0,  0, -1, 0], 
                   [0, 0, 0, 1]])
+    centroid_translate = np.eye(4)
+    centroid_translate[:3, 3] = centroid_source
+    inv_centroid_translate = np.eye(4)
+    inv_centroid_translate[:3, 3] = -centroid_source
+    
     #print(transformation)
-    transformation = transformation @ R
+    transformation = transformation @ (centroid_translate @ R @ inv_centroid_translate)
     merge_intrinsics_and_extrinsics(
         Path(args.target_camera_path),
         Path(args.source_camera_path),
